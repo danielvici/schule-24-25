cdf3f49ef44078bebd6d7289842dd00f0dc7b9dd
danielvici123 <94993276+danielvici@users.noreply.github.com>
2024-09-16 14:23:22 +0200

    Create normale.ino
    vorlage damit ich es später irgendwann kopieren und wieder einsetzen kann

---
+++ INF/vorlage/normale.ino/normale.ino
@@ -0,0 +1,23 @@
+/* 
+  Details
+*/
+// ---------- Libraries/Pins ---------- //
+
+
+// ---------- Variables ---------- //
+
+
+// ---------- Init ---------- //
+void setup() {
+  // Initialize
+}
+
+
+// ---------- Main Program ---------- //
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+// ---------- Functions ---------- //
