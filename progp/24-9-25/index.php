6bd6b1519584d4b2b7267db22142d7c63aa0e6d7
danielvici123 <94993276+danielvici@users.noreply.github.com>
2024-09-26 08:19:38 +0200

    Progp von 24-9-24
    Dateien von PROGP vom 9.25.24. Schwerpunkt Variablen und Einleitung in PHP

---
+++ progp/24-9-25/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Daniels Internetseite</title>
+</head>
+
+<body>
+	<p>Dies ist eine Zeile in HTML.</p><br>
+	<?php
+	echo "<h1>"."Hello World!"."</h1>"."<br>";
+	/* KOMMENTAR ÜBER 
+	MEHR ALS 
+	EINE ZEILE*/
+	echo "Das ist die zweite Zeile in PHP!"."<br>";
+	echo "Das ist die dritte Zeile in PHP!"."<br>";
+	echo "Das ist das Ende";
+	/* MERKE: Für PHP, Datei speichern in PErsöhnlicher Ordner, public_html 
+	mit Endung .php Dann mit Browser öffnen: localhost/~cwikladaniel/dateiname.php */
+	?>
+</body>
+</html>
---
+++ progp/24-9-25/variablen.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head> 
+<body>
+    <!-- Merke HABE ICH IN NOTION GEMACHT -->
+     <?php
+        $name="Cwikla";
+        $vorname="DANIEL";
+        echo "Guten Tag, "."$name "."$vorname"."<br>";
+        /* GANZZAHL */
+        $note = 1;
+        echo $note."<br>";
+        /* NEGATIVE GANZZAHL */
+        $negzahl = -2;
+        echo $negzahl."<br>";
+        /* Kommazahl */
+        $kommazahl=1.23;
+        echo $kommazahl."<br>";
+        /* Große Zahl*/
+        $grossezahl = 5.5e6;
+        echo $grossezahl."<br>";
+        /* */
+        $kleinzahl = 1.2e-3;
+        echo $kleinzahl."<br>";
+        /* */
+        /* ."<br>" */
+     ?>
+    
+</body>
+</html>
