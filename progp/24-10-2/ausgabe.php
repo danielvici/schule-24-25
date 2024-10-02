3fc3d02fe60d14d5778b570a95026de08ac28767
danielvici123 <94993276+danielvici@users.noreply.github.com>
2024-10-02 10:38:12 +0200

    aufgabe von 24-10-2 NICHT FERTIG

---
+++ progp/24-10-2/ausgabe.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Daten Ausgabe</h1>
+
+    <?php
+        // Variablen
+        $vorname= $_REQUEST['vorname'];
+        echo "Moin ".$vorname."! Du GEILE sau. <br>";
+    ?>
+</php>
+</body>
+</html>
