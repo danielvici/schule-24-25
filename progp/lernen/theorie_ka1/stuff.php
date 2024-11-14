e1f402dc6d3da9a9b480fb56a3b9bc7abe5c0400
danielvici123 <94993276+danielvici@users.noreply.github.com>
2024-11-14 10:03:28 +0100

    Ordner umbenannt und dinge wegen ka hinzugefügt

---
+++ progp/lernen/theorie_ka1/stuff.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        echo "Modulo: ". 44 % 12 . "<br>";
+
+        echo "!(()): " . !((13+27)==40) . "<br>";
+
+        echo "**" . 2**2 . "<br>";
+    ?>
+</body>
+</html>
