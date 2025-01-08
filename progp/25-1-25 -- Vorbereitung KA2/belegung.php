dde16df1b8c548f5a36c64238716a04ec402f616
danielvici123 <94993276+danielvici@users.noreply.github.com>
2025-01-08 14:29:16 +0100

    aufgabe prog 25-1-8

---
+++ progp/25-1-25 -- Vorbereitung KA2/belegung.php
@@ -0,0 +1,18 @@
+<?php 
+function sucheMinBelegung(zimmerliste: Zimmer[], weiblich: boolean): int {
+    $minIndex = -1;
+    $belegung = 1.0;
+    $minBelegung = 1;
+    for ($idx = 0; $idx < zimmerliste.length; $idx++) {
+        if ($zimmerliste[$idx].gibWeiblicheGeschlecht() == true){
+            $belegung = $zimmerliste[$idx].gibAnzahlGaeste() / $zimmerliste[$idx].gibAnzahlBetten();
+            if ($belegung < $minBelegung){
+                $minBelegung = $belegung;
+                $minIndex = $idx;
+            }
+        }
+
+    }
+}
+
+?>
